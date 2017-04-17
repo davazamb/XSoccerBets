9d533ec572cef18555f5d61d2aa6eb4ec4b55796
David Ali Zambrano Burgos <davazamb10@outlook.com>
2017-04-16 22:46:03 -0300

    Login and Inage Logo

---
+++ SoccerBets/SoccerBets/ViewModels/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoccerBets.ViewModels
+{
+    public class LoginViewModel
+    {
+        #region Properties
+        public LoginViewModel Login { get; set; }
+        #endregion
+        public LoginViewModel()
+        {
+            Login = new LoginViewModel();
+
+        }
+    }
+}
